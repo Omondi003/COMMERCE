4121ca46c5eb5c6f5d952d7b46f4c99b51b31c2a
Fai-chebby <chebetf034@gmail.com>
2024-07-14 23:24:45 +0300

    Backend project

---
+++ COMMERCES/src/main/java/org/example/commerces/CommercesApplication.java
@@ -0,0 +1,13 @@
+package org.example.commerces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommercesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommercesApplication.class, args);
+    }
+
+}
