4c4b1c7c82608a68505635a0ee06fb799a721944
Fai-chebby <chebetf034@gmail.com>
2024-07-16 23:06:14 +0300

    Commerce class details

--- COMMERCES/src/main/java/Commerce.java
+++ COMMERCES/src/main/java/Commerce.java
@@ -1,3 +1,14 @@
-public class Commerce {
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+ class Commerce {
+public String Id,
+    public String name,
+    public String Address,
+    public String phoneNumber,
+
+
 
 }
