35b876b59e5f75351ee3b0ffec552720c6815dc4
Fai-chebby <chebetf034@gmail.com>
2024-07-16 23:12:12 +0300

    Main class

--- COMMERCES/src/main/java/Commerce.java
+++ COMMERCES/src/main/java/Commerce.java
@@ -1,15 +1,17 @@
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
  class Commerce {
-public String Id,
-    public String name,
-    public String Address,
-    public String phoneNumber,
+public String Id;
+    public String name;
+    public String Address;
+    public String phoneNumber;
 
 
 
