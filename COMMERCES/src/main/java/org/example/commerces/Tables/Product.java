df59e0c43cecc1762b481377d75131d89c0c7053
Fai-chebby <chebetf034@gmail.com>
2024-10-07 10:47:52 +0300

    product entity

--- COMMERCES/src/main/java/org/example/commerces/Tables/Product.java
+++ COMMERCES/src/main/java/org/example/commerces/Tables/Product.java
@@ -1,6 +1,21 @@
 package org.example.commerces.Tables;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+@Entity
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private  String  id;
 
     private  String  product_name;
