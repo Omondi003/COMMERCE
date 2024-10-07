9184556a10c0b5e8dcc4156d4dc7b7592e8b5963
Fai-chebby <chebetf034@gmail.com>
2024-10-07 10:49:26 +0300

    Order_details table

---
+++ COMMERCES/src/main/java/org/example/commerces/Tables/Order_details.java
@@ -0,0 +1,11 @@
+package org.example.commerces.Tables;
+
+public class Order_details {
+    private  String  id;
+
+    private  String  product_name;
+
+    private String desc;
+
+    private String SKU;
+}
