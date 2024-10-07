508972d952dd3e99586c5490af13fd66989a27ec
Fai-chebby <chebetf034@gmail.com>
2024-10-07 10:25:00 +0300

    Address entity

--- COMMERCES/src/main/java/org/example/commerces/Tables/User_address.java
+++ COMMERCES/src/main/java/org/example/commerces/Tables/User_address.java
@@ -1,6 +1,20 @@
 package org.example.commerces.Tables;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
 public class User_address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private  String  id;
 
     private  String  user_id;
