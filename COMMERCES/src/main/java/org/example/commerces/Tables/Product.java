0db6c434cfb0a036115e633392094cf9e4f9df35
Fai-chebby <chebetf034@gmail.com>
2024-10-07 10:43:32 +0300

    Product table

---
+++ COMMERCES/src/main/java/org/example/commerces/Tables/Product.java
@@ -0,0 +1,18 @@
+package org.example.commerces.Tables;
+
+public class Product {
+    private  String  id;
+
+    private  String  user_id;
+
+    private String Address;
+
+    private String City;
+
+    private String Country;
+
+    private String Postal_code;
+
+
+    private String Telephone_no;
+}
