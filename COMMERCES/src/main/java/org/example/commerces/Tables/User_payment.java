f4f0598195e14b35dd01f32995f66f62897ac34c
Fai-chebby <chebetf034@gmail.com>
2024-10-07 10:39:54 +0300

    payment details

--- COMMERCES/src/main/java/org/example/commerces/Tables/User_payment.java
+++ COMMERCES/src/main/java/org/example/commerces/Tables/User_payment.java
@@ -1,6 +1,12 @@
 package org.example.commerces.Tables;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class User_payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private  String  id;
 
     private  String  user_id;
