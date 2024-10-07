bd9602d18c36fb16cfa89aa0a0744b1f687dd45a
Fai-chebby <chebetf034@gmail.com>
2024-10-07 11:29:16 +0300

    Discount table

---
+++ COMMERCES/src/main/java/org/example/commerces/Tables/Discount.java
@@ -0,0 +1,13 @@
+package org.example.commerces.Tables;
+
+public class Discount {
+    private  String  id;
+
+    private  String Name;
+
+    private String Desc;
+
+    private  String Discount_percent;
+
+    private String Active;
+}
