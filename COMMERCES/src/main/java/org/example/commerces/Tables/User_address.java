3275e3e79a6dcec44e35f4158ad038115028cdca
Fai-chebby <chebetf034@gmail.com>
2024-10-07 10:19:47 +0300

    user_address table

---
+++ COMMERCES/src/main/java/org/example/commerces/Tables/User_address.java
@@ -0,0 +1,18 @@
+package org.example.commerces.Tables;
+
+public class User_address {
+    private  String  id;
+
+    private  String  user_id;
+
+    private String Username;
+
+    private String password;
+
+    private String first_name;
+
+    private String last_name;
+
+
+    private String phoneNumber;
+}
