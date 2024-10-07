b516754a74931f7a57fd9bd746992cf4ad08f98e
Fai-chebby <chebetf034@gmail.com>
2024-10-07 11:31:36 +0300

    Discount table

--- COMMERCES/src/main/java/org/example/commerces/Tables/Discount.java
+++ COMMERCES/src/main/java/org/example/commerces/Tables/Discount.java
@@ -1,6 +1,21 @@
 package org.example.commerces.Tables;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
 public class Discount {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private  String  id;
 
     private  String Name;
