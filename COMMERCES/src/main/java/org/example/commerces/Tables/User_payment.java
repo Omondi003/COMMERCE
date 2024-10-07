57ca8b62d5a565a9fee29cfb72dfc9a12aae44af
Fai-chebby <chebetf034@gmail.com>
2024-10-07 10:38:41 +0300

    User_payment table

---
+++ COMMERCES/src/main/java/org/example/commerces/Tables/User_payment.java
@@ -0,0 +1,13 @@
+package org.example.commerces.Tables;
+
+public class User_payment {
+    private  String  id;
+
+    private  String  user_id;
+
+    private String Payment_type;
+
+    private String Card_type;
+
+    private String Account_no;
+}
