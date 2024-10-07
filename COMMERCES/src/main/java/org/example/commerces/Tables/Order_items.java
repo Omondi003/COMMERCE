10740bf01040d23415a5aacd4ff401c893067a03
Fai-chebby <chebetf034@gmail.com>
2024-10-07 10:53:49 +0300

    Order_items TABLE

---
+++ COMMERCES/src/main/java/org/example/commerces/Tables/Order_items.java
@@ -0,0 +1,11 @@
+package org.example.commerces.Tables;
+
+public class Order_items {
+    private  String  id;
+
+    private  String  User_id;
+
+    private String Total;
+
+    private String Payment_id;
+}
