645d833c50dcb846ae1b86e4998799108dde15ff
Fai-chebby <chebetf034@gmail.com>
2024-10-07 11:18:08 +0300

    Card_items

--- COMMERCES/src/main/java/org/example/commerces/Tables/Card_items.java
+++ COMMERCES/src/main/java/org/example/commerces/Tables/Card_items.java
@@ -1,6 +1,21 @@
 package org.example.commerces.Tables;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+@Entity
 public class Card_items {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private  String  id;
 
     private  String  Session_id;
