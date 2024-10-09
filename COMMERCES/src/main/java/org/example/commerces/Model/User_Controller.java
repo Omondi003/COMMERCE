1c37014636d852383d2c88d1910bac07ae17f158
Fai-chebby <chebetf034@gmail.com>
2024-10-09 10:59:19 +0300

    User_Controller

--- COMMERCES/src/main/java/org/example/commerces/Model/User_Controller.java
+++ COMMERCES/src/main/java/org/example/commerces/Model/User_Controller.java
@@ -3,12 +3,14 @@ package org.example.commerces.Model;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@Setter
 @RestController
 @RequestMapping("/commerce")
 public class User_Controller {
