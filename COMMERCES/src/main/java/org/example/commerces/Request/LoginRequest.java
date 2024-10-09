ef20e38b03ca521d5282d4ab372bf50492c3a5bf
Fai-chebby <chebetf034@gmail.com>
2024-10-09 10:38:39 +0300

    login

--- COMMERCES/src/main/java/org/example/commerces/Request/LoginRequest.java
+++ COMMERCES/src/main/java/org/example/commerces/Request/LoginRequest.java
@@ -1,5 +1,13 @@
 package org.example.commerces.Request;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginRequest {
     private String username;
 
