0c1e3a1a4b25b3fba84f35c70eb935c61efe3e07
Fai-chebby <chebetf034@gmail.com>
2024-10-09 10:42:50 +0300

    RegistrationRequest

--- COMMERCES/src/main/java/org/example/commerces/Request/RegistrationRequest.java
+++ COMMERCES/src/main/java/org/example/commerces/Request/RegistrationRequest.java
@@ -1,5 +1,12 @@
 package org.example.commerces.Request;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class RegistrationRequest {
     private String username;
 
