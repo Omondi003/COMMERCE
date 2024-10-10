f65cec196cbca0e92d2bcbc499c79e351216e896
Fai-chebby <chebetf034@gmail.com>
2024-10-10 21:13:32 +0300

    product

--- COMMERCES/src/main/java/org/example/commerces/Model/product.java
+++ COMMERCES/src/main/java/org/example/commerces/Model/product.java
@@ -1,4 +1,20 @@
 package org.example.commerces.Model;
 
 public class product {
+    private  String  id;
+
+    private  String  product_name;
+
+    private String desc;
+
+    private String SKU;
+
+    private String Category_id;
+
+    private String Inventory_id;
+
+    private String Price;
+
+    private String Discount_id;
+
 }
