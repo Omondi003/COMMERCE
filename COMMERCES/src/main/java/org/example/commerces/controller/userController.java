63c1f0ab48d89e6d4904b2b0ff5d1b1f6fb00e2c
Fai-chebby <chebetf034@gmail.com>
2024-10-15 22:40:11 +0300

    getmapping

--- COMMERCES/src/main/java/org/example/commerces/controller/userController.java
+++ COMMERCES/src/main/java/org/example/commerces/controller/userController.java
@@ -1,6 +1,7 @@
 package org.example.commerces.controller;
 
 import org.example.commerces.Model.User_model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class userController {
 
-    public User_model getUser_modelDetails(String username) {
-        return new User_model();
+    User_model user_model;
+    @GetMapping("{User_id}")
+    public User_model getUser_modelDetails(String User_id) {
+        return  user_model;
     }
 }
