181e8b6c95b7c67615c8baadcbc5e2241a572540
Fai-chebby <chebetf034@gmail.com>
2024-10-15 22:28:14 +0300

    userController

--- COMMERCES/src/main/java/org/example/commerces/controller/userController.java
+++ COMMERCES/src/main/java/org/example/commerces/controller/userController.java
@@ -1,5 +1,6 @@
 package org.example.commerces.controller;
 
+import org.example.commerces.Model.User_model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class userController {
 
+    public User_model getUser_modelDetails(String username) {
+        return new User_model();
+    }
 }
