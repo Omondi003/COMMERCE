320c722e10950d82fee958df7d40ab2da4e8b052
Fai-chebby <chebetf034@gmail.com>
2024-10-15 22:45:04 +0300

    Postmapping

--- COMMERCES/src/main/java/org/example/commerces/controller/userController.java
+++ COMMERCES/src/main/java/org/example/commerces/controller/userController.java
@@ -2,6 +2,7 @@ package org.example.commerces.controller;
 
 import org.example.commerces.Model.User_model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +14,10 @@ public class userController {
     @GetMapping("{User_id}")
     public User_model getUser_modelDetails(String User_id) {
         return  user_model;
+    }
+
+    public  String createUser_modelDetails(@RequestBody User_model user_model) {
+       this.user_model = user_model;
+        return "User_model created";
     }
 }
