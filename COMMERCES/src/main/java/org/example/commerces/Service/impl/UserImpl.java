5c88636f8f5214a20a3048b7c38a78845af87e9e
Fai-chebby <chebetf034@gmail.com>
2024-10-16 12:40:33 +0300

    adding constructors

--- COMMERCES/src/main/java/org/example/commerces/Service/impl/UserImpl.java
+++ COMMERCES/src/main/java/org/example/commerces/Service/impl/UserImpl.java
@@ -1,11 +1,15 @@
 package org.example.commerces.Service.impl;
 
+import lombok.AllArgsConstructor;
 import org.example.commerces.Model.User_model;
+import org.example.commerces.Repository.UserRepo;
 import org.example.commerces.Service.UserService;
 
 import java.util.List;
+@AllArgsConstructor
 
 public class UserImpl  implements UserService {
+    UserRepo userRepo;
     @Override
     public String createUser(User_model user_model) {
         return "";
