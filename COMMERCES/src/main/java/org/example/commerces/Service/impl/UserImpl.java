e1fe3ca504ac5a5c6a92b7204339daa095ca97e5
Fai-chebby <chebetf034@gmail.com>
2024-10-16 12:51:42 +0300

    service layer

--- COMMERCES/src/main/java/org/example/commerces/Service/impl/UserImpl.java
+++ COMMERCES/src/main/java/org/example/commerces/Service/impl/UserImpl.java
@@ -12,26 +12,30 @@ public class UserImpl  implements UserService {
     UserRepo userRepo;
     @Override
     public String createUser(User_model user_model) {
-        return "";
+        userRepo.save(user_model);
+        return "Success";
     }
 
     @Override
     public String updateUser(User_model user_model) {
-        return "";
+        userRepo.save(user_model);
+        return "Success";
     }
 
     @Override
     public String deleteUser(String user_id) {
-        return "";
+        userRepo.deleteById(user_id);
+        return "Success";
     }
 
     @Override
     public User_model getUser(String user_id) {
-        return null;
+
+        return userRepo.findById(user_id).get();
     }
 
     @Override
     public List<User_model> getAllUsers() {
-        return List.of();
+        return userRepo.findAll();
     }
 }
