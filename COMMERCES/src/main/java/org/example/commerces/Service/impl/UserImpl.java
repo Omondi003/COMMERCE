a5d1776790a97b7cea54de803893ca42d2bd07bd
Fai-chebby <chebetf034@gmail.com>
2024-10-16 12:37:50 +0300

    implementation

--- COMMERCES/src/main/java/org/example/commerces/Service/impl/UserImpl.java
+++ COMMERCES/src/main/java/org/example/commerces/Service/impl/UserImpl.java
@@ -1,4 +1,33 @@
 package org.example.commerces.Service.impl;
 
-public class UserImpl {
+import org.example.commerces.Model.User_model;
+import org.example.commerces.Service.UserService;
+
+import java.util.List;
+
+public class UserImpl  implements UserService {
+    @Override
+    public String createUser(User_model user_model) {
+        return "";
+    }
+
+    @Override
+    public String updateUser(User_model user_model) {
+        return "";
+    }
+
+    @Override
+    public String deleteUser(String user_id) {
+        return "";
+    }
+
+    @Override
+    public User_model getUser(String user_id) {
+        return null;
+    }
+
+    @Override
+    public List<User_model> getAllUsers() {
+        return List.of();
+    }
 }
