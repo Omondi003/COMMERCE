a7ede197e6ec8502dccf3361586ca3ea3fe8cd94
Fai-chebby <chebetf034@gmail.com>
2024-10-17 21:28:45 +0300

    ProductImpl

---
+++ COMMERCES/src/main/java/org/example/commerces/Service/impl/ProductImpl.java
@@ -0,0 +1,33 @@
+package org.example.commerces.Service.impl;
+
+import org.example.commerces.Model.Product;
+import org.example.commerces.Service.ProductService;
+
+import java.util.List;
+
+public class ProductImpl  implements ProductService {
+    @Override
+    public void createProduct(Product product) {
+
+    }
+
+    @Override
+    public void updateProduct(Product product) {
+
+    }
+
+    @Override
+    public void deleteProduct(Product product_id) {
+
+    }
+
+    @Override
+    public Product getProduct(String product_id) {
+        return null;
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
+        return List.of();
+    }
+}
