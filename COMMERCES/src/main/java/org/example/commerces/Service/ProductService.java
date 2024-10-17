668df0b78c6c00542c5594de9507bcf1cb959264
Fai-chebby <chebetf034@gmail.com>
2024-10-17 21:26:21 +0300

    ProductService

--- COMMERCES/src/main/java/org/example/commerces/Service/ProductService.java
+++ COMMERCES/src/main/java/org/example/commerces/Service/ProductService.java
@@ -1,4 +1,14 @@
 package org.example.commerces.Service;
 
+import org.example.commerces.Model.Product;
+import org.example.commerces.Model.User_model;
+
+import java.util.List;
+
 public interface ProductService {
+    public void createProduct(Product product);
+    public void updateProduct(Product product);
+    public void deleteProduct(Product product_id);
+    public Product getProduct(String product_id);
+    public List<Product> getAllProducts();
 }
--- COMMERCES/src/main/java/org/example/commerces/controller/ProductController.java
+++ COMMERCES/src/main/java/org/example/commerces/controller/ProductController.java
@@ -1,4 +1,40 @@
 package org.example.commerces.controller;
 
+import org.example.commerces.Model.Product;
+import org.example.commerces.Model.User_model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 public class ProductController {
+    Product product
+    @GetMapping("{Product_id}")
+    public User_model getProductDetails(@PathVariable("Product_id") String User_id) {
+
+        return .getUser(User_id);
+    }
+
+    @GetMapping()
+    public List<User_model> getAllUser_modelDetails() {
+
+        return userService.getAllUsers();
+    }
+
+    @PostMapping
+    public  String createUser_modelDetails(@RequestBody User_model user_model) {
+        userService.createUser(user_model);
+        return "User_model created";
+    }
+
+    @PutMapping
+    public  String updateUser_modelDetails(@RequestBody User_model user_model) {
+        userService.updateUser(user_model);
+        return "User_model updated";
+    }
+
+    @DeleteMapping({"User_id"})
+    public  String deleteUser_modelDetails(String User_id) {
+        userService.deleteUser(User_id);
+        return "User_model deleted";
+    }
 }
