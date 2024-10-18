44234ff51a52290fdcdcdc6bb8665f284ea45a96
Fai-chebby <chebetf034@gmail.com>
2024-10-18 23:21:55 +0300

    constructor

--- COMMERCES/src/main/java/org/example/commerces/Model/cart_model.java
+++ COMMERCES/src/main/java/org/example/commerces/Model/cart_model.java
@@ -1,4 +1,21 @@
 package org.example.commerces.Model;
 
+import jdk.jfr.Name;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class cart_model {
+    private  String  id;
+
+    private  String  Session_id;
+
+    private String Product_id;
+
+    private String quantity;
 }
