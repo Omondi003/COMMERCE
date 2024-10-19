e2a7bc2846852043fc725c3bcc6298a5c6c78f2f
Fai-chebby <chebetf034@gmail.com>
2024-10-19 22:52:02 +0300

    cart Service

--- COMMERCES/src/main/java/org/example/commerces/Service/impl/CartImpl.java
+++ COMMERCES/src/main/java/org/example/commerces/Service/impl/CartImpl.java
@@ -1,18 +1,45 @@
 package org.example.commerces.Service.impl;
 
 import lombok.AllArgsConstructor;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.example.commerces.Model.Cart_model;
+import org.example.commerces.Service.CartService;
+
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
-import java.util.function.Function;
+
+
 @AllArgsConstructor
 @Service
-public class CartImpl implements JpaRepository {
+public class CartImpl implements CartService {
 
+    @Override
+    public void createProduct(Cart_model cart_model) {
+
+    }
+
+    @Override
+    public void updateProduct(Cart_model cart_model) {
+
+    }
+
+    @Override
+    public void deleteProduct(Cart_model cart_model_id) {
+
+    }
+
+    @Override
+    public void deleteProduct(String cart_id) {
+
+    }
+
+    @Override
+    public Cart_model getProduct(String cart_id) {
+        return null;
+    }
+
+    @Override
+    public List<Cart_model> getAllProducts() {
+        return List.of();
+    }
 }
