12e0109dc6fe5c865cfe77abe464725ea1b45778
Fai-chebby <chebetf034@gmail.com>
2024-10-21 21:20:39 +0300

    cart

--- COMMERCES/src/main/java/org/example/commerces/Service/impl/CartImpl.java
+++ COMMERCES/src/main/java/org/example/commerces/Service/impl/CartImpl.java
@@ -2,6 +2,7 @@ package org.example.commerces.Service.impl;
 
 import lombok.AllArgsConstructor;
 import org.example.commerces.Model.Cart_model;
+import org.example.commerces.Repository.CartRepo;
 import org.example.commerces.Service.CartService;
 
 import org.springframework.stereotype.Service;
@@ -12,10 +13,11 @@ import java.util.List;
 @AllArgsConstructor
 @Service
 public class CartImpl implements CartService {
+    CartRepo cartRepo;
 
     @Override
     public void createProduct(Cart_model cart_model) {
-
+    cartRepo.save(cart_model);
     }
 
     @Override
