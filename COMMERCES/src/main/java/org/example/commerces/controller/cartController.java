64ca11f219b7f4db99b04eb59376ba990bffbd82
Fai-chebby <chebetf034@gmail.com>
2024-11-01 21:10:26 +0300

    cartController

--- COMMERCES/src/main/java/org/example/commerces/controller/cartController.java
+++ COMMERCES/src/main/java/org/example/commerces/controller/cartController.java
@@ -1,7 +1,10 @@
 package org.example.commerces.controller;
 
 import org.example.commerces.Model.Cart_model;
+import org.example.commerces.Model.Product_model;
 import org.example.commerces.Service.CartService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api")
 public class cartController {
    CartService cartService;
+   @GetMapping("/{CartId}")
+   public Cart_model getCartDetails(@PathVariable("CartId") String cartId) {
+
+      return cartService.getProduct(cartId);
+   }
+
 
 
 }
