c76d1e7e8f67add7b025334ac5cedd05a0c6fd36
Fai-chebby <chebetf034@gmail.com>
2024-11-02 21:29:57 +0300

    OrderController

--- COMMERCES/src/main/java/org/example/commerces/controller/OrderController.java
+++ COMMERCES/src/main/java/org/example/commerces/controller/OrderController.java
@@ -1,6 +1,9 @@
 package org.example.commerces.controller;
 
+import org.example.commerces.Model.Order_model;
 import org.example.commerces.Service.OrderService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class OrderController {
     OrderService orderService;
+    @GetMapping("Id")
+    public Order_model getOrderDetails(  @PathVariable ("Id") String Id) {
+        return orderService.getOrder(Id);
+
+    }
 }
