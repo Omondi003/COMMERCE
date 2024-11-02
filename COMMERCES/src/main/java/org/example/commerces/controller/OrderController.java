cc2fd36ab1a7f39c44caee1f0732afe6c0d48b48
Fai-chebby <chebetf034@gmail.com>
2024-11-02 21:24:32 +0300

    order controller

--- COMMERCES/src/main/java/org/example/commerces/controller/OrderController.java
+++ COMMERCES/src/main/java/org/example/commerces/controller/OrderController.java
@@ -1,4 +1,11 @@
 package org.example.commerces.controller;
 
+import org.example.commerces.Service.OrderService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
 public class OrderController {
+    OrderService orderService;
 }
