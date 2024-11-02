7571a9fd9a057d17e79726cdf372c14d7959399d
Fai-chebby <chebetf034@gmail.com>
2024-11-02 21:06:22 +0300

    cartmodel

--- COMMERCES/src/main/java/org/example/commerces/Model/Order_model.java
+++ COMMERCES/src/main/java/org/example/commerces/Model/Order_model.java
@@ -1,4 +1,20 @@
 package org.example.commerces.Model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
 public class Order_model {
+    private  String  id;
+
+    private  String  Session_id;
+
+    private String Product_id;
+
+    private String quantity;
 }
