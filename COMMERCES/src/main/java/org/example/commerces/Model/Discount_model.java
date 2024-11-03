562bbd016724159806939fa6ed78316de1833461
Fai-chebby <chebetf034@gmail.com>
2024-11-03 21:11:04 +0300

    model

--- COMMERCES/src/main/java/org/example/commerces/Model/Discount_model.java
+++ COMMERCES/src/main/java/org/example/commerces/Model/Discount_model.java
@@ -1,4 +1,13 @@
 package org.example.commerces.Model;
 
 public class Discount_model {
+    private  String  id;
+
+    private  String name;
+
+    private String description;
+
+    private  String discountPercent;
+
+    private String active;
 }
