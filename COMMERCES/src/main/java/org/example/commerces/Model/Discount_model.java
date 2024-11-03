2ed8fa813a765bf909bfb54af6edb5ea6798b814
Fai-chebby <chebetf034@gmail.com>
2024-11-03 21:12:09 +0300

    Model

--- COMMERCES/src/main/java/org/example/commerces/Model/Discount_model.java
+++ COMMERCES/src/main/java/org/example/commerces/Model/Discount_model.java
@@ -1,5 +1,14 @@
 package org.example.commerces.Model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Discount_model {
     private  String  id;
 
