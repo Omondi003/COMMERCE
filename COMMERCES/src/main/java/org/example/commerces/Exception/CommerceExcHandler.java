9b25edb0a0da3b3be7a26f6dd816f8365c79c7ee
Fai-chebby <chebetf034@gmail.com>
2024-11-04 17:04:40 +0300

    Exception

--- COMMERCES/src/main/java/org/example/commerces/Exception/CommerceExcHandler.java
+++ COMMERCES/src/main/java/org/example/commerces/Exception/CommerceExcHandler.java
@@ -1,4 +1,20 @@
 package org.example.commerces.Exception;
 
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+
 public class CommerceExcHandler {
+    public ResponseEntity<Object> handleNotFoundExc
+            ( NotFoundExc notFoundExc)
+    {
+        CommerceExc commerceExc= new CommerceExc(
+                notFoundExc.getMessage(),
+                notFoundExc.getCause(),
+                HttpStatus.NOT_FOUND
+
+        );
+        return new ResponseEntity<>(commerceExc, HttpStatus.NOT_FOUND);
+    }
 }
