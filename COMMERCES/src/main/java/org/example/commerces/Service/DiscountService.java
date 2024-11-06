ff0d2ff6515c7be252765ed025101d4133e34fae
Fai-chebby <chebetf034@gmail.com>
2024-11-06 18:18:38 +0300

    DiscountImpl

--- COMMERCES/src/main/java/org/example/commerces/Service/DiscountService.java
+++ COMMERCES/src/main/java/org/example/commerces/Service/DiscountService.java
@@ -3,9 +3,13 @@ package org.example.commerces.Service;
 import org.example.commerces.Model.Discount_model;
 import org.example.commerces.Tables.Discount;
 
+import java.util.List;
+
 public interface DiscountService {
     void createProduct(Discount_model discount_model);
     void updateProduct(Discount_model discount_model);
-    void deleteProduct(Discount_model discount_model_id );
+    void deleteProduct(String discount_id );
+    Discount_model getProduct(String discount_id);
+    List<Discount_model> getProducts();
 
 }
---
+++ COMMERCES/src/main/java/org/example/commerces/Service/impl/DiscountImpl.java
@@ -0,0 +1,34 @@
+package org.example.commerces.Service.impl;
+
+import org.example.commerces.Model.Discount_model;
+import org.example.commerces.Service.DiscountService;
+import org.example.commerces.Tables.Discount;
+
+import java.util.List;
+
+public class DiscountImpl implements DiscountService {
+    @Override
+    public void createProduct(Discount_model discount_model) {
+
+    }
+
+    @Override
+    public void updateProduct(Discount_model discount_model) {
+
+    }
+
+    @Override
+    public void deleteProduct(String discount_id) {
+
+    }
+
+    @Override
+    public Discount_model getProduct(String discount_id) {
+        return null;
+    }
+
+    @Override
+    public List<Discount_model> getProducts() {
+        return List.of();
+    }
+}
